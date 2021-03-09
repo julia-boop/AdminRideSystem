8422405bc734d960d130a9ca3e243d3c5781fe19
julia-boop <crdr.jc@gmail.com>
2021-03-09 18:30:03 -0300

    configuracion solo admin

--- src/app.js
+++ src/app.js
@@ -19,6 +19,6 @@ app.use('/user', userRouter);
 app.use('/concurso', concursoRouter);
 
 
-app.listen(3000, function(){
+app.listen(3002, function(){
     console.log('el servidor esta corriendoooooooo :)')
 })
